58234dc6435111ae864d69b0face4f8d987ab2df
gabriel carrillo <MrArizona@users.noreply.github.com>
2018-08-23 12:35:14 -0700

    Created News Fragment & News Recycler

---
+++ app/src/main/java/io/curio/android/kotlinreddit/NewsFragment.kt
@@ -0,0 +1,10 @@
+package io.curio.android.kotlinreddit
+
+import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+
+class NewsFragment: Fragment(){
+
+    private var newsList: RecyclerView? = null
+
+}
---
+++ app/src/main/java/io/curio/android/kotlinreddit/NewsRecycler.kt
@@ -0,0 +1,5 @@
+package io.curio.android.kotlinreddit
+
+class NewsRecycler{
+
+}
