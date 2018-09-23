77402a1715d334447c1d5d7fb64bcae81102dd12
Gabriel Carrillo <MrArizona@users.noreply.github.com>
2018-09-23 15:05:23 -0700

    Created Extensions & Update NewsRecycler

---
+++ app/src/main/java/io/curio/android/kotlinreddit/Extensions.kt
@@ -0,0 +1,9 @@
+package io.curio.android.kotlinreddit
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+fun ViewGroup.inflate(layoutId: Int, attatchToRoot: Boolean = false): View
+    = LayoutInflater.from(context).inflate(layoutId,this,attatchToRoot)
+
--- app/src/main/java/io/curio/android/kotlinreddit/NewsFragment.kt
+++ app/src/main/java/io/curio/android/kotlinreddit/NewsFragment.kt
@@ -1,10 +1,25 @@
 package io.curio.android.kotlinreddit
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 class NewsFragment: Fragment(){
 
     private var newsList: RecyclerView? = null
+/**
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(R.layout.news_fragment, container, false)
+        newsList = view.findViewById(R.id.news_list) as RecyclerView?
+        newsList?.setHasFixedSize(true) // use this setting to improve performance
+        newsList?.layoutManager = LinearLayoutManager(context)
+
+        return view
+    }
+    **/
 
 }
