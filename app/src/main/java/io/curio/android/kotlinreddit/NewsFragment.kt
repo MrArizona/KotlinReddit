9c68fee7b751ce276b8fd3c048a02bfdd5237592
Gabriel Carrillo <MrArizona@users.noreply.github.com>
2018-09-27 16:51:51 -0700

    Added News Fragment layout

--- app/src/main/java/io/curio/android/kotlinreddit/NewsFragment.kt
+++ app/src/main/java/io/curio/android/kotlinreddit/NewsFragment.kt
@@ -11,15 +11,15 @@ import android.view.ViewGroup
 class NewsFragment: Fragment(){
 
     private var newsList: RecyclerView? = null
-/**
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.news_fragment, container, false)
+        val view = inflater.inflate(R.layout.fragment_news, container, false)
         newsList = view.findViewById(R.id.news_list) as RecyclerView?
         newsList?.setHasFixedSize(true) // use this setting to improve performance
         newsList?.layoutManager = LinearLayoutManager(context)
 
         return view
     }
-    **/
+
 
 }
